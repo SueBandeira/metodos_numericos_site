6518b7899a5ccf674851d7aa2f00d44fdf9cdec6
Herick2D <contato.herickmoreira@gmail.com>
2025-02-01 18:01:57 -0300

    minor-fix: identação

--- src/main/java/com/metodos/metodos_numericos/metodos/MetodosService.java
+++ src/main/java/com/metodos/metodos_numericos/metodos/MetodosService.java
@@ -7,167 +7,166 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class MetodosService { // todo adicionar os métodos e condições devidas para guia no controller
 
-  private double funcao(double x) {
-    return x * x - 4;
-  }
+    private double funcao(double x) {
+        return x * x - 4;
+    }
 
-  private double derivada(double x) {
-    return 2 * x;
+    private double derivada(double x) {
+        return 2 * x;
 
-  }
+    }
 
-  /*
-  ⌈============================⌉
+/*
+⌈============================⌉
         DADOS DE ENTRADA:
-  —————————————————————————————
-  Matriz A = [1,3;4,9]
-  Matriz b = [3;4]
-  —————————————————————————————
-  Matriz expandida: Matriz 1
- ⌈============================⌉
+—————————————————————————————
+    Matriz A = [1,3;4,9]
+    Matriz b = [3;4]
+—————————————————————————————
+    Matriz expandida: Matriz 1
+⌈============================⌉
         1ª Iteração
- ⌊============================⌋
+⌊============================⌋
 
-   L2 ⇐ L2 - L1 × (a21/a11)
+    L2 ⇐ L2 - L1 × (a21/a11)
 
- Matriz 2
+    Matriz 2
 
-m1:
-1 3 | 3
-4 9 | 4
-m2:
-1 3  |  3
-0 -3 | -8
-s:
--5.0000
-2.6667
+    m1:
+    1 3 | 3
+    4 9 | 4
+    m2:
+    1 3  |  3
+    0 -3 | -8
+    s:
+    -5.0000
+    2.6667
  */
 
-  public double sum(double num1, double num2) {
-    return num1 + num2;
-  } // método de exemplo para teste de endpoint
+    public double sum(double num1, double num2) {
+        return num1 + num2;
+    } // método de exemplo para teste de endpoint
 
-  public double calcularRaizBissecao(double a, double b, double tolerancia, int maxInteracoes) {
+    public double calcularRaizBissecao(double a, double b, double tolerancia, int maxInteracoes) {
 
-    double fa = funcao(a);
-    double fb = funcao(b);
+        double fa = funcao(a);
+        double fb = funcao(b);
+        if (fa * fb >= 0) {
+            throw new IllegalArgumentException("O método da bisseção não pode ser aplicado neste intervalo");
+        }
 
-    if (fa * fb >= 0) {
-      throw new IllegalArgumentException("O método da bisseção não pode ser aplicado neste intervalo");
-    }
+        double c = 0;
 
-    double c = 0;
+        for (int i = 0; i < maxInteracoes; i++) {
+            c = (a + b) / 2;
+            double fc = funcao(c);
 
-    for (int i = 0; i < maxInteracoes; i++) {
-      c = (a + b) / 2;
-      double fc = funcao(c);
+            if (Math.abs(fc) < tolerancia) {
+                return c;
+            }
 
-      if (Math.abs(fc) < tolerancia) {
+            if (fa * fc < 0) {
+                b = c;
+                fb = fc;
+            } else {
+                a = c;
+                fa = fc;
+            }
+        }
         return c;
-      }
-
-      if (fa * fc < 0) {
-        b = c;
-        fb = fc;
-      } else {
-        a = c;
-        fa = fc;
-      }
     }
-    return c;
-  }
 
-  public double calcularRaizSecante(double a, double b, double tolerancia, int maxInteracoes) {
+    public double calcularRaizSecante(double a, double b, double tolerancia, int maxInteracoes) {
 
-    double fa = funcao(a);
-    double fb = funcao(b);
+        double fa = funcao(a);
+        double fb = funcao(b);
 
-    for (int i = 0; i < maxInteracoes; i++) {
+        for (int i = 0; i < maxInteracoes; i++) {
 
-      if (Math.abs(fb) < tolerancia) {
+            if (Math.abs(fb) < tolerancia) {
+                return b;
+            }
+
+            double b2 = b - (fb * (b - a)) / (fb - fa);
+            a = b;
+            fa = fb;
+            b = b2;
+            fb = funcao(b);
+        }
         return b;
-      }
-
-      double b2 = b - (fb * (b - a)) / (fb - fa);
-      a = b;
-      fa = fb;
-      b = b2;
-      fb = funcao(b);
-    }
-    return b;
-  }
-
-  public double calcularRaizNewtonRapshon(double a, double tolerancia, int maxInteracoes) {
-
-    double b = a;
-
-    for (int i = 0; i < maxInteracoes; i++) {
-      double fb = funcao(b);
-      double dfb = derivada(b);
-
-      if (Math.abs(dfb) < 1e-10) {
-        throw new ArithmeticException("Derivada próxima de zero. O método não pode continuar.");
-      }
-
-      double bProximo = b - (fb / dfb);
-
-      if (Math.abs(bProximo - b) < tolerancia) {
-        return bProximo;
-      }
-      b = bProximo;
-    }
-    return b;
-  }
-
-  public double[] calcularEliminacaoGaussiana(double[][] a, double[] b) {
-    int n = a.length;
-
-    // Criando a matriz aumentada [A|B]
-    double[][] matrixAumentada = new double[n][n + 1];
-    for (int i = 0; i < n; i++) {
-      System.arraycopy(a[i], 0, matrixAumentada[i], 0, n);
-      matrixAumentada[i][n] = b[i]; // Adiciona a coluna de termos independentes
     }
 
-    // Transformação para a forma triangular superior
-    for (int k = 0; k < n; k++) {
-      // Pivoteamento parcial
-      int maxRow = k;
-      for (int i = k + 1; i < n; i++) {
-        if (Math.abs(matrixAumentada[i][k]) > Math.abs(matrixAumentada[maxRow][k])) {
-          maxRow = i;
+    public double calcularRaizNewtonRapshon(double a, double tolerancia, int maxInteracoes) {
+
+        double b = a;
+
+        for (int i = 0; i < maxInteracoes; i++) {
+            double fb = funcao(b);
+            double dfb = derivada(b);
+
+            if (Math.abs(dfb) < 1e-10) {
+                throw new ArithmeticException("Derivada próxima de zero. O método não pode continuar.");
+            }
+
+            double bProximo = b - (fb / dfb);
+
+            if (Math.abs(bProximo - b) < tolerancia) {
+                return bProximo;
+            }
+            b = bProximo;
         }
-      }
-      // Troca de linhas se necessário
-      double[] temp = matrixAumentada[k];
-      matrixAumentada[k] = matrixAumentada[maxRow];
-      matrixAumentada[maxRow] = temp;
+        return b;
+    }
 
-      // Verifica se o sistema tem solução única
-      if (matrixAumentada[k][k] == 0) {
-        throw new IllegalArgumentException("O sistema não tem solução única.");
-      }
+    public double[] calcularEliminacaoGaussiana(double[][] a, double[] b) {
+        int n = a.length;
 
-      // Eliminação
-      for (int i = k + 1; i < n; i++) {
-        double fator = matrixAumentada[i][k] / matrixAumentada[k][k];
-        for (int j = k; j <= n; j++) { // Vai até n para incluir a coluna de termos independentes
-          matrixAumentada[i][j] -= fator * matrixAumentada[k][j];
+        // Criando a matriz aumentada [A|B]
+        double[][] matrixAumentada = new double[n][n + 1];
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(a[i], 0, matrixAumentada[i], 0, n);
+            matrixAumentada[i][n] = b[i]; // Adiciona a coluna de termos independentes
         }
-      }
-    }
 
-    // Resolução por substituição regressiva
-    double[] x = new double[n];
-    for (int i = n - 1; i >= 0; i--) {
-      x[i] = matrixAumentada[i][n];
-      for (int j = i + 1; j < n; j++) {
-        x[i] -= matrixAumentada[i][j] * x[j];
-      }
-      x[i] /= matrixAumentada[i][i];
-    }
+        // Transformação para a forma triangular superior
+        for (int k = 0; k < n; k++) {
+            // Pivoteamento parcial
+            int maxRow = k;
+            for (int i = k + 1; i < n; i++) {
+                if (Math.abs(matrixAumentada[i][k]) > Math.abs(matrixAumentada[maxRow][k])) {
+                    maxRow = i;
+                }
+            }
+            // Troca de linhas se necessário
+            double[] temp = matrixAumentada[k];
+            matrixAumentada[k] = matrixAumentada[maxRow];
+            matrixAumentada[maxRow] = temp;
 
-    return x;
-  }
+            // Verifica se o sistema tem solução única
+            if (matrixAumentada[k][k] == 0) {
+                throw new IllegalArgumentException("O sistema não tem solução única.");
+            }
+
+            // Eliminação
+            for (int i = k + 1; i < n; i++) {
+                double fator = matrixAumentada[i][k] / matrixAumentada[k][k];
+                for (int j = k; j <= n; j++) { // Vai até n para incluir a coluna de termos independentes
+                    matrixAumentada[i][j] -= fator * matrixAumentada[k][j];
+                }
+            }
+        }
+
+        // Resolução por substituição regressiva
+        double[] x = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            x[i] = matrixAumentada[i][n];
+            for (int j = i + 1; j < n; j++) {
+                x[i] -= matrixAumentada[i][j] * x[j];
+            }
+            x[i] /= matrixAumentada[i][i];
+        }
+
+        return x;
+    }
 
 }
--- src/main/java/com/metodos/metodos_numericos/metodos/metodosController.java
+++ src/main/java/com/metodos/metodos_numericos/metodos/metodosController.java
@@ -14,113 +14,113 @@ import org.springframework.web.server.ResponseStatusException;
 @AllArgsConstructor
 public class metodosController {
 
-  private final MetodosService metodosService;
+    private final MetodosService metodosService;
 
-  @GetMapping("/soma")
-  public ResponseEntity<Double> sum(@RequestParam double num1, @RequestParam double num2) { // endpoint de exemplo
-    double resultado = metodosService.sum(num1, num2);
-    return ResponseEntity.ok(resultado);
-  }
-
-  @GetMapping("/bisseccao")
-  public ResponseEntity<Double> encontrarRaizBissecao(
-      @RequestParam double a,
-      @RequestParam double b,
-      @RequestParam(defaultValue = "0.000001") double tolerancia,
-      @RequestParam(defaultValue = "100") int maxInteracoes) {
-
-    try {
-      double raiz = metodosService.calcularRaizBissecao(a, b, tolerancia, maxInteracoes);
-      return ResponseEntity.status(HttpStatus.OK).body(raiz);
-    } catch (IllegalArgumentException e) {
-      throw new IllegalStateException(e.getMessage());
+    @GetMapping("/soma")
+    public ResponseEntity<Double> sum(@RequestParam double num1, @RequestParam double num2) { // endpoint de exemplo
+        double resultado = metodosService.sum(num1, num2);
+        return ResponseEntity.ok(resultado);
     }
-  }
 
-  @GetMapping("/secante")
-  public ResponseEntity<Double> encontrarRaizSecante(
-      @RequestParam double a,
-      @RequestParam double b,
-      @RequestParam(defaultValue = "0.000001") double tolerancia,
-      @RequestParam(defaultValue = "100") int maxInteracoes) {
+    @GetMapping("/bisseccao")
+    public ResponseEntity<Double> encontrarRaizBissecao(
+            @RequestParam double a,
+            @RequestParam double b,
+            @RequestParam(defaultValue = "0.000001") double tolerancia,
+            @RequestParam(defaultValue = "100") int maxInteracoes) {
 
-    try {
-      double raiz = metodosService.calcularRaizSecante(a, b, tolerancia, maxInteracoes);
-      return ResponseEntity.status(HttpStatus.OK).body(raiz);
-    } catch (IllegalArgumentException e) {
-      throw new IllegalStateException(e.getMessage());
-    }
-  }
-
-  @GetMapping("/newton-rapshon")
-  public ResponseEntity<Double> encontrarRaizNewtonRapshon(
-      @RequestParam double a,
-      @RequestParam(defaultValue = "0.000001") double tolerancia,
-      @RequestParam(defaultValue = "100") int maxInteracoes) {
-
-    try {
-      double raiz = metodosService.calcularRaizNewtonRapshon(a, tolerancia, maxInteracoes);
-      return ResponseEntity.status(HttpStatus.OK).body(raiz);
-    } catch (IllegalArgumentException e) {
-      throw new IllegalStateException(e.getMessage());
-    }
-  }
-
-  @GetMapping("/eliminacao-gaussiana")
-  public ResponseEntity<double[]> encontrarSolucaoEG(
-      @RequestParam(defaultValue = "3,4;7,8") String a,
-      @RequestParam(defaultValue = "3;4") String b
-  ) {
-    try {
-      double[][] matrizA = converterParaMatriz(a);
-      double[] matrizB = converterParaVetor(b);
-
-      if (matrizA.length != matrizB.length) {
-        throw new IllegalArgumentException("O número de linhas da matriz A deve ser igual ao tamanho do vetor B.");
-      }
-
-      double[] raiz = metodosService.calcularEliminacaoGaussiana(matrizA, matrizB);
-      return ResponseEntity.ok(raiz);
-    } catch (IllegalArgumentException e) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-    } catch (Exception e) {
-      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao processar a solicitação.");
-    }
-  }
-
-  private double[][] converterParaMatriz(String matrizStr) {
-    try {
-      String[] linhas = matrizStr.split(";");
-      int numLinhas = linhas.length;
-      int numColunas = linhas[0].split(",").length;
-
-      double[][] matriz = new double[numLinhas][numColunas];
-      for (int i = 0; i < numLinhas; i++) {
-        String[] valores = linhas[i].split(",");
-        if (valores.length != numColunas) {
-          throw new IllegalArgumentException("Todas as linhas da matriz devem ter o mesmo número de colunas.");
+        try {
+            double raiz = metodosService.calcularRaizBissecao(a, b, tolerancia, maxInteracoes);
+            return ResponseEntity.status(HttpStatus.OK).body(raiz);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException(e.getMessage());
         }
-        for (int j = 0; j < numColunas; j++) {
-          matriz[i][j] = Double.parseDouble(valores[j]);
-        }
-      }
-      return matriz;
-    } catch (Exception e) {
-      throw new IllegalArgumentException("Formato inválido para matriz A. Use formato: \"3,4;7,8\"");
     }
-  }
 
-  private double[] converterParaVetor(String vetorStr) {
-    try {
-      String[] valores = vetorStr.split(";");
-      double[] vetor = new double[valores.length];
-      for (int i = 0; i < valores.length; i++) {
-        vetor[i] = Double.parseDouble(valores[i]);
-      }
-      return vetor;
-    } catch (Exception e) {
-      throw new IllegalArgumentException("Formato inválido para vetor B. Use formato: \"3;4\"");
+    @GetMapping("/secante")
+    public ResponseEntity<Double> encontrarRaizSecante(
+            @RequestParam double a,
+            @RequestParam double b,
+            @RequestParam(defaultValue = "0.000001") double tolerancia,
+            @RequestParam(defaultValue = "100") int maxInteracoes) {
+
+        try {
+            double raiz = metodosService.calcularRaizSecante(a, b, tolerancia, maxInteracoes);
+            return ResponseEntity.status(HttpStatus.OK).body(raiz);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException(e.getMessage());
+        }
     }
-  }
+
+    @GetMapping("/newton-rapshon")
+    public ResponseEntity<Double> encontrarRaizNewtonRapshon(
+            @RequestParam double a,
+            @RequestParam(defaultValue = "0.000001") double tolerancia,
+            @RequestParam(defaultValue = "100") int maxInteracoes) {
+
+        try {
+            double raiz = metodosService.calcularRaizNewtonRapshon(a, tolerancia, maxInteracoes);
+            return ResponseEntity.status(HttpStatus.OK).body(raiz);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException(e.getMessage());
+        }
+    }
+
+    @GetMapping("/eliminacao-gaussiana")
+    public ResponseEntity<double[]> encontrarSolucaoEG(
+            @RequestParam(defaultValue = "3,4;7,8") String a,
+            @RequestParam(defaultValue = "3;4") String b
+    ) {
+        try {
+            double[][] matrizA = converterParaMatriz(a);
+            double[] matrizB = converterParaVetor(b);
+
+            if (matrizA.length != matrizB.length) {
+                throw new IllegalArgumentException("O número de linhas da matriz A deve ser igual ao tamanho do vetor B.");
+            }
+
+            double[] raiz = metodosService.calcularEliminacaoGaussiana(matrizA, matrizB);
+            return ResponseEntity.ok(raiz);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao processar a solicitação.");
+        }
+    }
+
+    private double[][] converterParaMatriz(String matrizStr) {
+        try {
+            String[] linhas = matrizStr.split(";");
+            int numLinhas = linhas.length;
+            int numColunas = linhas[0].split(",").length;
+
+            double[][] matriz = new double[numLinhas][numColunas];
+            for (int i = 0; i < numLinhas; i++) {
+                String[] valores = linhas[i].split(",");
+                if (valores.length != numColunas) {
+                    throw new IllegalArgumentException("Todas as linhas da matriz devem ter o mesmo número de colunas.");
+                }
+                for (int j = 0; j < numColunas; j++) {
+                    matriz[i][j] = Double.parseDouble(valores[j]);
+                }
+            }
+            return matriz;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Formato inválido para matriz A. Use formato: \"3,4;7,8\"");
+        }
+    }
+
+    private double[] converterParaVetor(String vetorStr) {
+        try {
+            String[] valores = vetorStr.split(";");
+            double[] vetor = new double[valores.length];
+            for (int i = 0; i < valores.length; i++) {
+                vetor[i] = Double.parseDouble(valores[i]);
+            }
+            return vetor;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Formato inválido para vetor B. Use formato: \"3;4\"");
+        }
+    }
 
 }
